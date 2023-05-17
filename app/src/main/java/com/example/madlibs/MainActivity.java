568314130ca15e2bacf74377e57f017da3e1ae15
Steve Marquez <201009954@INV509349.lbusd.local>
2023-05-17 11:50:36 -0700

    Initial commit

---
+++ app/src/main/java/com/example/madlibs/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.madlibs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText etNoun;
+    private EditText etAdverb;
+    private EditText etAdjective;
+    private EditText etColor;
+    private EditText etAnimal;
+    private EditText etFavoriteColor;
+    private EditText etAnyNumber;
+    private EditText etToy;
+    private EditText etCar;
+    private EditText etResturant;
+    private EditText etAmountOfMoney;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
