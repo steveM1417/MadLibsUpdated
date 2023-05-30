70bc3d3f2ca3418af68b495eeabdc66fa838e350
201009954 <stevemarquez777@gmail.com>
2023-05-30 11:52:33 -0700

    almost done

--- app/src/main/java/com/example/madlibs/MainActivity.java
+++ app/src/main/java/com/example/madlibs/MainActivity.java
@@ -9,7 +9,8 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText etBaseballTeam;
+    private EditText etTrueOrFalse;
     private EditText etNoun;
     private EditText etAdverb;
     private EditText etAdjective;
@@ -30,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        etBaseballTeam = findViewById(R.id.et_BaseballTeam);
+        etTrueOrFalse = findViewById(R.id.et_TrueOrFalse);
         etNoun = findViewById(R.id.et_Noun);
         etAdverb = findViewById(R.id.et_Adverb);
         etAdjective = findViewById(R.id.et_Adjective);
@@ -48,11 +51,19 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 String story = "";
-                story += "\nI went to the " + etNoun.getText() + " and I had a " + etAnimal.getText() + ".";
-                story += "\n...";
-                story += "\n...";
-                story += "\n...";
-                story += "\n...";
+                story += "\nAre you scared of the dark? " + etTrueOrFalse.getText();
+                story += "\nIt is " + etTrueOrFalse.getText() + " that you believe in ghost? ";
+                story += "\nIt was a " + etAdjective.getText() + " type of night with you and some friends. ";
+                story += "\nIn the distance you hear a " + etAnimal.getText() + ". Its crying out to the sky. ";
+                story += "\nYou begin to " + etAdverb.getText() + " look for it. ";
+                story += "\nThe screams are coming from a " + etColor.getText() + " house. ";
+                story += "\nYou feel attracted to this house and take a look inside. ";
+                story += "\nAs soon as you walk in you see " + etNumber.getText() + " of broken " + etToy.getText() + ".";
+                story += "\nOh nah this place gives me the creeps yells out one of your friends. ";
+                story += "\nYou walk back out and pick up a " +  etBaseballTeam.getText() + " next to the " + etNoun.getText() + ".";
+                story += "\nYou go to the street and see a " + etColor.getText() + etCar.getText() +" parked. ";
+                story += "\nYou check if its open and see the keys already in the ignition. ";
+                story += "\nYour friends are hungry from adventuring so you stop at " + etResturant.getText() + ".";
                 story += "\n...";
                 story += "\n...";
                 story += "\n...";
